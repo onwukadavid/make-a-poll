385da183d5ab72a0a1df0517098a8819f66542c3
onwukadavid <onwukadavid21@gmail.com>
2024-01-12 17:16:37 +0100

    Update: Changed Poll to Question and added extra fields

--- Polls/urls.py
+++ Polls/urls.py
@@ -6,6 +6,6 @@ app_name = 'polls'
 urlpatterns = [
     path('create-poll/', views.create_poll, name='create-poll'),
     path('polls/', views.all_polls, name='all-polls'),
-    path('polls/<slug:slug>', views.view_poll, name='view-poll'),
-    path('polls/<slug:slug>', views.delete_poll, name='delete-poll'),
+    path('polls/<str:username>/<slug:slug>', views.view_poll, name='view-poll'),
+    path('polls/<str:username>/<slug:slug>', views.delete_poll, name='delete-poll'),
 ]
