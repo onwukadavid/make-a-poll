94a65bc7c697966e77c3755c96fe0708eb2d3c98
onwukadavid <onwukadavid21@gmail.com>
2024-01-17 21:02:01 +0100

    update: add votes attribute to choice model update: customize django admin form form poll

--- Polls/admin.py
+++ Polls/admin.py
@@ -1,7 +1,22 @@
 from django.contrib import admin
 from Polls.models import Question, Choice
 
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    min_num = 2
+    max_num = 4
+
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['title', 'description', 'question', 'pub_date', 'updated_at', 'status', 'slug', 'thumbnail']
+    # fields = ['title', 'description', 'question', 'pub_date', 'updated_at', 'status', 'slug', 'thumbnail', 'user']
+
+    fieldsets = [
+        ('Poll information', {"fields":['title', 'description', 'status', 'slug', 'question']}),
+        ('Date information', {"fields":['pub_date', 'updated_at']}),
+    ]
+
+    inlines = [ChoiceInline]
+
+    
 
 admin.site.register(Question, QuestionAdmin)
