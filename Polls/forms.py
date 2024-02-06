3a3ea2ebf6a1bf80690f9b8dae0eb29ca0038f04
David Truman <onwukadavid21@gmail.com>
2024-02-06 21:09:52 +0100

    feat: add create poll feature through question poll form, create poll view and template

--- Polls/forms.py
+++ Polls/forms.py
@@ -1,8 +1,15 @@
 from django import forms
 
-class QuestionForm(forms.Forms):
+
+STATUS = {
+    'published':'PUBLISHED',
+    'draft':'DRAFT'
+}
+
+
+class QuestionForm(forms.Form):
     title = forms.CharField(max_length=50)
-    description = forms.CharField(max_length=50)
-    thumbnail = forms.ImageField()
+    description = forms.CharField(widget=forms.Textarea)
+    thumbnail = forms.ImageField(required=False)
     question = forms.CharField(max_length=255)
-    status = forms.CharField()
+    status = forms.ChoiceField(choices=STATUS, widget=forms.Select)
--- Polls/views.py
+++ Polls/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 from Polls.models import Question
 from django.shortcuts import get_object_or_404
+from Polls.forms import QuestionForm
 
-def create_poll():
-    ...
+def create_poll(request):
+    context = {}
+
+    if request.method == 'POST':
+        poll_form = QuestionForm(request.POST, request.FILES)
+        if poll_form.is_valid():
+            Question.objects.create(
+                user = request.user,
+                title = poll_form.cleaned_data.get('title'),
+                description = poll_form.cleaned_data.get('description'),
+                thumbnail = poll_form.cleaned_data.get('thumbnail'),
+                question = poll_form.cleaned_data.get('question'),
+                status = poll_form.cleaned_data.get('status'),
+            )
+        else:
+            context['error'] = "Form contains errors"
+    else:
+        poll_form = QuestionForm()
+    
+    context['poll_form'] = poll_form
+
+    return render(request, 'Polls/create_poll.html', context)
 
 def view_poll(request, username, slug):
     
