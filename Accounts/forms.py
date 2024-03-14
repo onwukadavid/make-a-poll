3be0caee894947c01bd7443585d497b8930b90d8
David Truman <onwukadavid21@gmail.com>
2024-03-14 23:41:32 +0100

    update: create user registration and update forms in forms.py

--- Accounts/forms.py
+++ Accounts/forms.py
@@ -3,6 +3,44 @@ from models import Author
 from django.contrib.auth.forms import ReadOnlyPasswordHashField
 from django.core.exceptions import ValidationError
 
+
+# Create Registeration form
+class UserRegistrationForm(forms.Form):
+    username = forms.CharField(max_length=50)
+    email = forms.EmailField(max_length=100)
+    password = forms.PasswordInput()
+    password2 = forms.PasswordInput()
+
+    def clean_password2(self):
+        password1 = self.cleaned_data['password'] 
+        password2 = self.cleaned_data['password2']
+
+        if password1 and password2 and password2 != password1:
+            raise ValidationError('Passwords do not match')
+        return password2
+
+# create Login form if necessary
+
+# create update form if necessary
+class UserUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Author
+        fields = ['username', 'email']
+
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        user = Author.objects.filter(username=username)
+
+        if user.exists():
+            raise ValidationError('This username has been taken')
+        
+    def clean_email(self):
+        email = self.cleaned_data['email']
+        user = Author.objects.filter(email=email)
+
+        if user.exists():
+            raise ValidationError('This email already exists')
+        
 class UserCreationForm(forms.ModelForm):
     """A form for creating new users. Includes all the required
     fields, plus a repeated password."""
